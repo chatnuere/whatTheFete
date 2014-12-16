f28e83be7be32f806b4a6ad375387764925c9d4b
chatnuere <dugue.pierrejean@gmail.com>
2014-12-16 14:19:22 +0100

    Blank project with FB connect

---
+++ What The Fête/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  What The Fête
+//
+//  Created by Pierre-Jean DUGUÉ on 16/12/2014.
+//  Copyright (c) 2014 Pierre-Jean DUGUÉ. All rights reserved.
+//
+
+#ifndef What_The_Fe_te_Bridge_Header_h
+#define What_The_Fe_te_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
---
+++ What The Fête/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  What The Fête
+//
+//  Created by Pierre-Jean DUGUÉ on 16/12/2014.
+//  Copyright (c) 2014 Pierre-Jean DUGUÉ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, FBLoginViewDelegate {
+    
+    @IBOutlet var fbLoginView : FBLoginView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+    
+    
+    // Facebook Delegate Methods
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        println("User ID: \(user.objectID)")
+        println("User Name: \(user.name)")
+        var userEmail = user.objectForKey("email") as String
+        println("User Email: \(userEmail)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
