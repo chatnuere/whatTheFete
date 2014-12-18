097cf6b591e895037cbee0efbc105c9f8da81a56
Remy Dragyn <remy.dragyn@gmail.com>
2014-12-19 00:47:16 +0100

    cmieux

--- What The Fête/SoireeTableViewCell.swift
+++ What The Fête/SoireeTableViewCell.swift
@@ -12,7 +12,8 @@ class SoireeTableViewCell: UITableViewCell {
 
     @IBOutlet weak var soireeTitle: UILabel!
     @IBOutlet weak var soireeDescription: UILabel!
-    @IBOutlet weak var soireeDate: UILabel!
+    @IBOutlet weak var soireeDateMonth: UILabel!
+    @IBOutlet weak var soireeDateDay: UILabel!
     @IBOutlet weak var soireeCoverpic: UIImageView!
     
     override func awakeFromNib() {
@@ -29,7 +30,59 @@ class SoireeTableViewCell: UITableViewCell {
     func setSoiree(soiree:Soiree) {
         soireeTitle.text        = soiree.title
         soireeDescription.text  = soiree.soireeDescription
-        soireeDate.text         = soiree.date
+        
+        let splitedDate         = split(soiree.date){$0 == "-"}
+        let soireeDay           = splitedDate[0]
+        
+        var soireeMonth : String
+        
+        switch splitedDate[1] {
+        case "01" :
+            soireeMonth         = "JAN"
+            break
+        case "02" :
+            soireeMonth         = "FEV"
+            break
+        case "03" :
+            soireeMonth         = "MAR"
+            break
+        case "04" :
+            soireeMonth         = "AVR"
+            break
+        case "05" :
+            soireeMonth         = "MAI"
+            break
+        case "06" :
+            soireeMonth         = "JUN"
+            break
+        case "07" :
+            soireeMonth         = "JUL"
+            break
+        case "08" :
+            soireeMonth         = "AOU"
+            break
+        case "09" :
+            soireeMonth         = "SEP"
+            break
+        case "10" :
+            soireeMonth         = "OCT"
+            break
+        case "11" :
+            soireeMonth         = "NOV"
+            break
+        case "12" :
+            soireeMonth         = "DEC"
+            break
+        default   :
+            soireeMonth         = "No"
+            break
+            
+        }
+        
+        
+        
+        soireeDateMonth.text    = soireeMonth
+        soireeDateDay.text      = soireeDay
         
         
         //Construct the imgUrl to get an image URL for the pages
